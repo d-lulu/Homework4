95f665aacfa47538101564f0e8885c9a4b103911
Алина <lukanovaalina974@gmail.com>
2025-10-05 15:04:32 +0300

    Add files via upload

---
+++ Homework4.c
@@ -0,0 +1,15 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <locale.h>
+
+main() {
+
+	setlocale(LC_ALL, "RU");
+
+	int A, B, condition;
+	printf("Данные полученные от датчиков traffic flow: ");
+	scanf("%d %d", &A, &B);
+	condition = ((A % 2 == 1) && (B % 2 != 1)) || ((A % 2 != 1) && (B % 2 == 1));
+	printf("Поворот налево? (1 - да, 0 - нет): %d", condition);
+
+}
