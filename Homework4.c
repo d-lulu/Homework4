5ef40698b3519ddcca611153c93f53d609efeee0
Алина <lukanovaalina974@gmail.com>
2025-10-06 21:20:13 +0300

    Update Homework4.c

--- Homework4.c
+++ Homework4.c
@@ -1,16 +1,21 @@
-﻿#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <locale.h>
 
-int main() {
+main() {
 
 	setlocale(LC_ALL, "RU");
 
+	// Сообщаем пользователю, что делает программа
+	printf("=== КУДА СЛЕДУЕТ ПОВЕРНУТЬ ===\n");
 	int A, B, condition;
 	printf("Данные полученные от датчиков traffic flow: ");
+	// Считываем два кода, введенные пользователем
 	scanf("%d %d", &A, &B);
+
+	// Вычисляем логическое условие	
 	condition = ((A % 2 == 1) && (B % 2 != 1)) || ((A % 2 != 1) && (B % 2 == 1));
 	printf("Поворот налево? (1 - да, 0 - нет): %d", condition);
 
-
 }
+
